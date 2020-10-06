e20d5a56cfb9dbe18fdf6e1b231e7a7a2aaa66e6
ceerik <asdert458@gmail.com>
2020-10-06 15:21:27 +0200

    Warehouse wip

--- src/online_shop/WareHouse.java
+++ src/online_shop/WareHouse.java
@@ -3,17 +3,21 @@ package online_shop;
 import java.util.ArrayList;
 import java.util.List;
 
+<<<<<<< Updated upstream
 import online_shop.product.Catagory;
 import online_shop.product.Manufacturer;
 import online_shop.product.Product;
 import online_shop.product.WeightProduct;
+=======
+import online_shop.product.*;
+>>>>>>> Stashed changes
 
 /*
  * This class will store the products and manage the quantity of each product.
- * 
- * TODO: build the class.
+ *
  */
 public class WareHouse {
+<<<<<<< Updated upstream
 	
 	private List<Product> allProducts;
 	
@@ -72,4 +76,52 @@ public class WareHouse {
 		*/
 		
 	}
+=======
+	private ArrayList<Product> stock;
+	private String stockPath;
+
+	public WareHouse(String stockPath) {
+		this.stockPath = stockPath;
+		loadStock();
+	}
+
+	public ArrayList<Product> getStock() {
+		return stock;
+	}
+
+	public void setStock(ArrayList<Product> stock) {
+		this.stock = stock;
+	}
+
+	// Changes the stock of weighted products.
+	public void changeItemStock(WeightProduct product, double stockChange) {
+		product.setWeight(product.getWeight() + stockChange);
+	}
+
+	// Changes the stock of non-weighted products.
+	public void changeItemStock(StockProduct product, int stockChange) {
+		product.setAmount(product.getAmount() + stockChange);
+	}
+
+	public void loadStock() {
+		List<String> productStrings = FileManager.getInstance().readFromFile(stockPath);
+		stock = new ArrayList<Product>();
+
+		for (String productString : productStrings) {
+			String[] productRow = productString.split(";");
+			//if the length of productRow is equal to 6 that means it is a "WeightProduct"
+			if (productRow.length == 6) {
+				stock.add(new WeightProduct(productRow[0], productRow[1], Manufacturer.getManufacturer(productRow[2]),
+						Catagory.getCatagory(productRow[3]), Double.parseDouble(productRow[4]), Double.parseDouble(productRow[5])));
+			} else {
+				stock.add(new Product(productRow[0], productRow[1], Manufacturer.getManufacturer(productRow[2]),
+						Catagory.getCatagory(productRow[3]), Double.parseDouble(productRow[4])));
+			}
+		}
+	}
+
+	public void saveStock() {
+
+	}
+>>>>>>> Stashed changes
 }
